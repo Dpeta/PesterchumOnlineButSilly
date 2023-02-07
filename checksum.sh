6c92c93a4e817eac9dee00f7ab1b4b5622070fc7
Dpeta <69427753+Dpeta@users.noreply.github.com>
2023-02-07 18:56:01 +0100

    Example hash layout

--- checksum.sh
+++ checksum.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+echo Checksum of dist/pesterchum.js:
 cat dist/pesterchum.js | openssl dgst -sha256 -binary | openssl base64 -A && echo
+echo Checksum of style.css:
+cat style.css | openssl dgst -sha256 -binary | openssl base64 -A && echo
