0427193040bfb033fc406c45479ac82c7d5c473f
laaledesiempre <alejandrabotacura@outlook.com.ar>
2024-03-01 10:04:40 -0300

    [CONFLICT] Resolved conflict

--- update_checksums.js
+++ update_checksums.js
@@ -2,18 +2,10 @@ const crypto = require('crypto');
 
 const fs = require("fs");
 
-<<<<<<< HEAD
-const buff = fs.readFileSync("./dist/pesterchum.js","utf8");
-const hash = crypto.createHash("sha256").update(buff).digest("binary");
-//base64
-base64= Buffer.from(hash,"binary").toString('base64')
-console.log(base64)
-=======
 const noBabelJsRegex=/<script src="dist\/pesterchum\.js" integrity="sha256-[^"]*" defer><\/script>/gm
 const noBabelCssRegex=/<link rel="stylesheet" integrity="sha256-[^"]*" href="style\.css">/gm
 const babeljsRegex=/<link rel="preload" href="style\.css" integrity="sha256-[^"]*" as="style">/gm
 const babelCssRegex=/<link rel="preload" href="dist\/pesterchum\.js" integrity="sha256-[^"]*" as="script">/gm
->>>>>>> 437fa81b8642b3f21ca8d14404977de540d896df
 
 const hashFile=(path)=>{
   const buff = fs.readFileSync(path,"utf8");
@@ -37,14 +29,7 @@ fs.readFile('./index.html', 'utf-8',  (err, contents) => {
   replaced= replaced.replace(noBabelCssRegex,`<link rel="stylesheet" integrity="sha256-v${cssHash}" href="style.css">`)
   replaced= replaced.replace(noBabelJsRegex,`<script src="dist/pesterchum.js" integrity="sha256-${jsHash}" defer></script>`)
 
-<<<<<<< HEAD
-  //writeFile('./example.txt', replaced, 'utf-8', function (err) {
-  //  console.log(err);
-  //});
-});*/
-=======
   fs.writeFile('./index.html', replaced, 'utf-8', function (err) {
     console.log(err);
   });
 });
->>>>>>> 437fa81b8642b3f21ca8d14404977de540d896df
