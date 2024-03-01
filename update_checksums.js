93382f75163fe560c973f7e53f6420920b8ad630
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2024-03-01 09:52:52 -0300

    [DEV] replacement function for testing

--- update_checksums.js
+++ update_checksums.js
@@ -2,30 +2,34 @@ const crypto = require('crypto');
 
 const fs = require("fs");
 
-const buff = fs.readFileSync("./src/pesterchum.js");
-const hash = createHash("sha256").update(buff).digest("hex");
-console.log(hash);
-//base64
-base64= Buffer.from(hash).toString('base64')
-console.log(base64)
+const noBabelJsRegex=/<script src="dist\/pesterchum\.js" integrity="sha256-[^"]*" defer><\/script>/gm
+const noBabelCssRegex=/<link rel="stylesheet" integrity="sha256-[^"]*" href="style\.css">/gm
+const babeljsRegex=/<link rel="preload" href="style\.css" integrity="sha256-[^"]*" as="style">/gm
+const babelCssRegex=/<link rel="preload" href="dist\/pesterchum\.js" integrity="sha256-[^"]*" as="script">/gm
 
+const hashFile=(path)=>{
+  const buff = fs.readFileSync(path);
+  const hash = createHash("sha256").update(buff).digest("hex");
+  base64= Buffer.from(hash).toString('base64')
+  return base64 
+}
+// here we have finally the hashes
+jsHash=hashFile("./src/pesterchum.js")
+cssHash=hashFile("./style.css")
 
-
-
-
-
-
-/*
-//replacing function
-readFile('./example.txt', 'utf-8', function (err, contents) {
+//replacing function non tested
+fs.readFile('./index.html', 'utf-8',  (err, contents) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(contents)
-  //const replaced = contents.replace(/to be replaced/g, 'replacement string');
+  
+  let replaced = contents.replace(babelCssRegex, `<link rel="preload" href="style.css" integrity="sha256-${cssHash}" as="style">`);
+  replaced= replaced.replace(babelJsRegex,`<link rel="preload" href="dist/pesterchum.js" integrity="sha256-${jsHash}" as="script">`)
+  replaced= replaced.replace(noBabelCssRegex,`<link rel="stylesheet" integrity="sha256-v${cssHash}" href="style.css">`)
+  replaced= replaced.replace(noBabelJsRegex,`<script src="dist/pesterchum.js" integrity="sha256-${jsHash}" defer></script>`)
 
-  //writeFile('./example.txt', replaced, 'utf-8', function (err) {
-  //  console.log(err);
-  //});
-});*/
+  fs.writeFile('./index.html', replaced, 'utf-8', function (err) {
+    console.log(err);
+  });
+});
